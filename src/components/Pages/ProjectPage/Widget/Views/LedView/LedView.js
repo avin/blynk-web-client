0756ae20615c441076fa8269938840653b1a74c0
avin <carcinogen75@gmail.com>
2019-02-17 15:31:40 +0300

    scale led view

--- src/components/Pages/ProjectPage/Widget/Views/LedView/LedView.js
+++ src/components/Pages/ProjectPage/Widget/Views/LedView/LedView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import SizeMe from '@avinlab/react-size-me';
 import WidgetLabel from '../../WidgetLabel/WidgetLabel';
 import styles from './styles.module.scss';
 import { pinValueSelector } from '../../../../../../redux/selectors';
@@ -15,17 +16,24 @@ export class LedView extends React.Component {
             <>
                 <WidgetLabel title={widget.get('label') || 'Led'} />
                 <div className={styles.ledContainer}>
-                    <svg width={24} height={24}>
-                        <circle
-                            r={10}
-                            cx={12}
-                            cy={12}
-                            className={styles.ledCircle}
-                            stroke={color}
-                            fill={color}
-                            fillOpacity={value / 255}
-                        />
-                    </svg>
+                    <SizeMe style={{ textAlign: 'center' }}>
+                        {({ width, height }) => {
+                            const size = Math.min(width, height) || 5;
+                            return (
+                                <svg width={size} height={size}>
+                                    <circle
+                                        r={Math.max(size / 2 - 4, 1)}
+                                        cx={size / 2}
+                                        cy={size / 2}
+                                        className={styles.ledCircle}
+                                        stroke={color}
+                                        fill={color}
+                                        fillOpacity={value / 255}
+                                    />
+                                </svg>
+                            );
+                        }}
+                    </SizeMe>
                 </div>
             </>
         );
